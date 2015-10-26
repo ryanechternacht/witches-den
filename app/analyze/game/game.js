a604f690b69bfeb96c4e1b79128877295b627fa3
ryanechternacht <ryan@echternacht.org>
2015-10-26 14:57:21 -0400

    Getting a file served to angular and calling data from it on page

--- app/analyze/game/game.js
+++ app/analyze/game/game.js
@@ -1,12 +1,20 @@
 'use strict';
 
-angular.module('myApp.analyze', ['ngRoute'])
+angular.module('myApp.analyze', ['ngRoute', 'ngResource'])
+
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/analyze/game', {
         templateUrl: 'analyze/game/game.html', 
         controller: 'AnalyzeGameCtrl'
     });
 }])
-.controller('AnalyzeGameCtrl', [function() { 
 
+.factory('AnalyzeGameSrv', ['$resource', function($resource){
+    return $resource('data/:game.json', {}, {
+        get: {method:'GET'}
+    });
+}])
+
+.controller('AnalyzeGameCtrl', ['$scope', 'AnalyzeGameSrv', function($scope, AnalyzeGameSrv) { 
+    $scope.gameLog = AnalyzeGameSrv.get({game:'game1'});
 }]);
