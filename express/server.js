e3aae1f845490142247c4e6c8dc84f168d290fe8
ryanechternacht <ryan@echternacht.org>
2015-12-05 10:09:33 -0500

    modified server.js to use the port from azure (from: http://stackoverflow.com/questions/19294203/node-js-express-only-work-on-port-3000-on-test-server)

--- express/server.js
+++ express/server.js
@@ -21,9 +21,10 @@ app.all("/*", function(req, res, next) {
         res.sendFile("index.html", { root: __dirname + "/../app" });
 });
 
+var port = process.env.port || 3001;
 
-app.listen(3001);
+app.listen(port);
 
 
 
-console.log('Listening on port 3001...');
+console.log('Listening on port ' + port + '...');
