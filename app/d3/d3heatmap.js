af4b5fcc1129ce3d15306bae0a41d1612bf3cab7
ryanechternacht <ryan@echternacht.org>
2016-04-24 20:10:00 -0400

    Making the heatmap even better

--- app/d3/d3heatmap.js
+++ app/d3/d3heatmap.js
@@ -16,7 +16,7 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
     svg.attr("width", width)
         .attr("height", height);
 
-    var margin = { top: 100, bottom: 30, left: 100, right: 30};
+    var margin = { top: 100, bottom: 30, left: 110, right: 30};
     
     var labelPadding = 10,
         yBottom = height - margin.bottom,
@@ -28,7 +28,7 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
 
     var yScale = d3.scale.ordinal()
         .domain(factions.map(x => x))
-        .rangeRoundBands([yTop, yBottom], .1);
+        .rangeRoundBands([yTop, yBottom], .0);
 
     var xScale = d3.scale.ordinal()
         .domain(factions.map(x => x))
@@ -41,8 +41,7 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
         .attr("transform", (d, i) => {
             var y = yScale(i);
             return "translate(" + xLeft + "," + y + ")";
-        })
-        .attr("class", "row");
+        });
 
     var cells = rows.selectAll("g")
         .data(d => d)
@@ -57,18 +56,54 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
         .attr("width", xScale.rangeBand())
         .attr("height", yScale.rangeBand())
         .attr("class", (d, i) => {
-            if(d == "-" || d == "") {
+            if(d.length == 0) { // when a color plays itself
+                if(factions[i] == "nomads" || factions[i] == "fakirs") {
+                    return "yellow-cell";
+                } else if(factions[i] == "chaosmagicians" || factions[i] == "giants") {
+                    return "red-cell";
+                } else if(factions[i] == "engineers" || factions[i] == "dwarves") {
+                    return "gray-cell";
+                } else if(factions[i] == "swarmlings" || factions[i] == "mermaids") {
+                    return "blue-cell";
+                } else if(factions[i] == "darklings" || factions[i] == "alchemists") {
+                    return "black-cell";
+                } else if(factions[i] == "cultists" || factions[i] == "halflings") {
+                    return "brown-cell";
+                } else if(factions[i] == "witches" || factions[i] == "auren") {
+                    return "green-cell";
+                }
+            } else if(d == "-") {
                 return "neutral";
-            } else if(d >= 75) {
-                return "strong-positive";
-            } else if(d >= 60) {
-                return "weak-positive";
-            } else if(d <= 25) {
-                return "strong-negative";
-            } else if(d <= 40) {
-                return "weak-negative";
+            } else if(d > 50) {
+                return "positive-cell";
+            } else if (d < 50) {
+                return "negative-cell"
             } else {
                 return "neutral";
+            }
+            // else if(d >= 75) {
+            //     return "strong-positive";
+            // } else if(d >= 60) {
+            //     return "weak-positive";
+            // } else if(d <= 25) {
+            //     return "strong-negative";
+            // } else if(d <= 40) {
+            //     return "weak-negative";
+            // } else {
+            //     return "neutral";
+            // }
+        })
+        .attr("style", d => {
+            // if positive, the closer to 100, the darker (50 is white)
+            // if negative, the closer to 0, the darker (50 is white)
+            // we add .2 to move values close to 50 to either cooler faster, 
+            // this also makes the darkest color happen at 80 or 20, which is okay
+            if(d > 50) {
+                return "fill-opacity: " + ((d-50) / 50 + .2);
+            } else if(d < 50) {
+                return "fill-opacity: " + ((50-d) / 50 + .2);
+            } else {
+                return "";
             }
         });
 
@@ -79,50 +114,56 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
         .attr("class", "cell-text");
 
 
-    var dividers = svg.selectAll(".dividers")
+    var dividers = svg.selectAll(".col-divider")
         .data(factions)
         .enter()
         .append("line")
             .attr("x1", (d,i) => {
                 if(i % 2 == 0) { // left side
-                    return xScale(i) + margin.left + 2;
+                    return xScale(i) + xLeft + 0;
                 } else { // right side
-                    return xScale(i) + xScale.rangeBand() + margin.left - 2;
+                    return xScale(i) + xScale.rangeBand() + xLeft - 0;
                 }
             })
             .attr("x2", (d,i) => {
                 if(i % 2 == 0) { // left side
-                    return xScale(i) + margin.left + 2;
+                    return xScale(i) + xLeft + 0;
                 } else { // right side
-                    return xScale(i) + xScale.rangeBand() + margin.left - 2;
+                    return xScale(i) + xScale.rangeBand() + xLeft - 0;
                 }
             })
-            .attr("y1", margin.top)
-            .attr("y2", chartHeight + margin.top)
-            .attr("class", (d,i) => {
-                if(factions[i] == "nomads" || factions[i] == "fakirs") {
-                    return "yellow";
-                } else if(factions[i] == "chaosmagicians" || factions[i] == "giants") {
-                    return "red";
-                } else if(factions[i] == "engineers" || factions[i] == "dwarves") {
-                    return "gray";
-                } else if(factions[i] == "swarmlings" || factions[i] == "mermaids") {
-                    return "blue";
-                } else if(factions[i] == "darklings" || factions[i] == "alchemists") {
-                    return "black";
-                } else if(factions[i] == "cultists" || factions[i] == "halflings") {
-                    return "brown";
-                } else if(factions[i] == "witches" || factions[i] == "auren") {
-                    return "green";
+            .attr("y1", yTop)
+            .attr("y2", yBottom)
+            .attr("class", "col-divider");
+
+        var dividers = svg.selectAll(".row-divider")
+        .data(factions)
+        .enter()
+        .append("line")
+            .attr("x1", xLeft)
+            .attr("x2", xRight)
+            .attr("y1", (d,i) => {
+                if(i % 2 == 0) { // top
+                    return yScale(i) + 0;
+                } else { // right side
+                    return yScale(i) + yScale.rangeBand() - 0;
                 }
-            });
+            })
+            .attr("y2", (d,i) => {
+                if(i % 2 == 0) { // top
+                    return yScale(i) + 0;
+                } else { // right side
+                    return yScale(i) + yScale.rangeBand() - 0;
+                }
+            })
+            .attr("class", "row-divider");
 
     // labels
     var leftLabels = svg.selectAll(".left-label")
         .data(factions)
         .enter()
         .append("text")
-        .text(d => d)
+        .text(d => translator(d))
         .attr('x', 0)
         .attr('y', d => yScale(d) + yScale.rangeBand() / 2 + 1)
         .attr("class", "row-header");
@@ -139,7 +180,7 @@ var drawHeatmap = function(d3, svg, scope, iElement, iAttrs) {
             })
             .attr("class", "top-label")
             .append("text")
-                .text(d => d)
+                .text(d => translator(d))
                 .attr("class", "column-header")
                 .attr("transform", d => "rotate(-70)");
 }
